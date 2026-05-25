31d6fa54c1235557a6853b4580b2063052ec8f48
KaVin S <kavinadithya3@gmail.com>
2026-05-25 09:05:33 +0530

    Learning JWT

--- src/main/java/com/techcrack/todoApi/security/SecurityConfiguration.java
+++ src/main/java/com/techcrack/todoApi/security/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,15 +16,15 @@ public class SecurityConfiguration {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
-				.csrf(csrf -> csrf.disable())
+				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
 				.httpBasic(Customizer.withDefaults())
-				.sessionManagement(session ->
-				    session.sessionCreationPolicy(
-					        SessionCreationPolicy.STATELESS
-					    )
-					)
-				.headers(headerOptions -> headerOptions.frameOptions(frame -> frame.disable()))
+				.sessionManagement(session -> session.sessionCreationPolicy(
+					SessionCreationPolicy.STATELESS
+				))
+				.headers(headerOptions -> headerOptions.frameOptions(
+						HeadersConfigurer.FrameOptionsConfig::disable
+				))
 				.build();
 	}
 }
